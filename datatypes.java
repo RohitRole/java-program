f260ec099ab5ee238f67a9c375d44ea7042373e4
RohitRole <145689440+RohitRole@users.noreply.github.com>
2026-01-31 15:39:16 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,24 @@
+public class car {
+    byte a = 10 ;
+    int b = 20 ;
+    short c = 30 ;
+    public static void main (String[]args) {
+        car a1 = new car () ;
+        car b1 = new car () ;
+        car c1 = new car () ;
+        long d = 10000000 ;
+        float e = 10.21f ;
+        double f = 10.210 ;
+        char g = 'R' ;
+        boolean h = ((a1.a)<(b1.b)) ;
+        
+        System.out.println(a1.a) ;
+         System.out.println(b1.b) ;
+          System.out.println(c1.c) ;
+           System.out.println(d) ;
+            System.out.println(e) ;
+             System.out.println(f) ;
+              System.out.println(g) ;
+               System.out.println(h) ;
+    }
+}
